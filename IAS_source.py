c9647bdaf3e56335df788dbdc72dc989bd4a919e
Ashen Hermit <zaghoul1012@gmail.com>
2025-11-27 14:43:32 +0700

    Update IAS_source.py

--- IAS_source.py
+++ IAS_source.py
@@ -102,7 +102,9 @@ def init_db():
             {'STT': 8, 'Ngày': pd.to_datetime('2025-01-17'), 'MaHS': 'HS001', 'Loại': 'Hoạt động', 'Nội dung': 'Giúp đỡ bạn bè', 'Điểm': 3, 'Tuần': 3},
             {'STT': 9, 'Ngày': pd.to_datetime('2025-01-18'), 'MaHS': 'HS001', 'Loại': 'Hoạt động', 'Nội dung': 'Tham gia CLB', 'Điểm': 5, 'Tuần': 3},
             {'STT': 10, 'Ngày': pd.to_datetime('2025-01-14'), 'MaHS': 'HS001', 'Loại': 'Vi phạm', 'Nội dung': 'Không làm bài tập', 'Điểm': 5, 'Tuần': 3},
-            {'STT': 11, 'Ngày': pd.to_datetime('2025-01-15'), 'MaHS': 'HS001', 'Loại': 'Hoạt động', 'Nội dung': 'Tham gia CLB', 'Điểm': 5, 'Tuần': 3}
+            {'STT': 11, 'Ngày': pd.to_datetime('2025-01-15'), 'MaHS': 'HS001', 'Loại': 'Hoạt động', 'Nội dung': 'Tham gia CLB', 'Điểm': 5, 'Tuần': 3},
+            {'STT': 12, 'Ngày': pd.to_datetime('2025-01-15'), 'MaHS': 'HS001', 'Loại': 'Vi phạm', 'Nội dung': 'Không làm bài tập', 'Điểm': 5, 'Tuần': 3},
+            {'STT': 12, 'Ngày': pd.to_datetime('2025-01-15'), 'MaHS': 'HS001', 'Loại': 'Vi phạm', 'Nội dung': 'Không trực nhật', 'Điểm': 5, 'Tuần': 3}
         ]
     
         # 2. Tạo DataFrame từ dữ liệu mẫu
@@ -566,6 +568,7 @@ with st.sidebar:
 
 if st.session_state['current_page'] == 'dashboard': render_ias_dashboard_page()
 else: render_data_management_page()
+
 
 
 
